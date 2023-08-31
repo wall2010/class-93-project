9523159254c59e5dad17cf998497a86fc93b3d26
wall2010 <122789274+wall2010@users.noreply.github.com>
2023-08-31 15:42:53 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,12 @@
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "kwitter_room.html";
+}
+function addUse()
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "index.html";
+}
